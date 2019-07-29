9c9b2d4861dc93a5fe7211f1a9f90fe5608d8f93
Cagri277 <xcorpius2004@gmail.com>
2019-07-28 22:43:30 -0400

    secondproj

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-let fdr = 'Erhan';
-let mdr = 'Ayse';
-let br1 = 'Osman';
-let br1s = 'Nurullah';
-let br2s = 'Numan';
-let br2= 'Ahmet';
-let me = 'Cagri';
-let mes = 'Fatih';
-let br3 = 'Furkan';
-let br3s = 'Efe';
-let lastname = 'Ozdemir';
-
-console.log(fdr + ' ' + lastname);
-console.log(mdr + ' ' + lastname);
-console.log(br1 + ' ' + br1s + ' ' + lastname);
-console.log(br2 + ' ' + br2s + ' ' + lastname);
-console.log(me + ' ' + mes + ' ' + lastname);
-console.log(br3 + ' ' + br3s + ' ' + lastname);
+let person1 = {
+    name: 'Erhan',
+    lastname: 'Ozdemir',
+    age: '56',
+};
+let person2 = {
+    name: 'Ayse',
+    lastname: 'Ozdemir',
+    age: '49',
+}
+let person3 = {
+    name: 'Osman',
+    secondname: 'Nurullah',
+    lastname: 'Ozdemir',
+    Age: '30',
+}
+let person4 = {
+    name: 'Ahmet',
+    secondname: 'Numan',
+    lastname: 'Ozdemir',
+    age: '28',
+}
+let person5 = {
+    name: 'Cagri',
+    secondname: 'Fatih',
+    lastname: 'Ozdemir',
+    Age: '15',
+}
+let person6 = {
+    name: 'Furkan',
+    secondname: 'Efe',
+    lastname: 'Ozdemir',
+    age: '11',
+}
+console.log(person1);
+console.log(person2);
+console.log(person3);
+console.log(person4);
+console.log(person5);
+console.log(person6);
