a17e35b824af229579ddf6428f0f4af46685c041
Cagri277 <xcorpius2004@gmail.com>
2019-07-30 12:54:35 -0400

    fizzbuzz

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-function getPersonDetails(person) {
-    return 'Name: ' + person.name + ' ' + person.secondname + ' ' + person.lastname + ' , Age: ' + person.age;
+function fizzbuzz (number) {
+    if (number % 15 === 0) {
+        return 'fizzbuzz'
+    }
+    {
+        if (number % 5 === 0){
+            return 'buzz'
+        }
+    }
+    {
+        if (number % 3 === 0){
+            return 'fizz'
+        }
+    } 
 }
-
-let erhan = {
-    name: 'Erhan',
-    secondname: '',
-    lastname: 'Ozdemir',
-    age: '56',
-};
-let ayse = {
-    name: 'Ayse',
-    secondname: '',
-    lastname: 'Ozdemir',
-    age: '49',
-}
-let osman = {
-    name: 'Osman',
-    secondname: 'Nurullah',
-    lastname: 'Ozdemir',
-    age: '30',
-}
-let numan = {
-    name: 'Ahmet',
-    secondname: 'Numan',
-    lastname: 'Ozdemir',
-    age: '28',
-}
-let cagri = {
-    name: 'Cagri',
-    secondname: 'Fatih',
-    lastname: 'Ozdemir',
-    age: '15',
-}
-let efe = {
-    name: 'Furkan',
-    secondname: 'Efe',
-    lastname: 'Ozdemir',
-    age: '11',
-}
-
-console.log(getPersonDetails(osman));
-console.log(getPersonDetails(numan));
-console.log(getPersonDetails(cagri));
-console.log(getPersonDetails(efe));
-console.log(getPersonDetails(ayse));
-console.log(getPersonDetails(erhan));
+console.log(fizzbuzz(30))
