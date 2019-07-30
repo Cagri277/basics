db04c7b0576b62c52e245bfd30e4551ad0fbd1bd
Cagri277 <xcorpius2004@gmail.com>
2019-07-30 12:57:14 -0400

    fizzbuzzv2

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function fizzbuzz (number) {
     if (number % 15 === 0) {
         return 'fizzbuzz'
     }
-    {
         if (number % 5 === 0){
             return 'buzz'
         }
-    }
-    {
         if (number % 3 === 0){
             return 'fizz'
-        }
-    } 
+        } 
 }
 console.log(fizzbuzz(30))
+console.log(fizzbuzz(9))
+console.log(fizzbuzz(10))
