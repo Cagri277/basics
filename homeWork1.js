01d901e013e89e59affdc45aa49edb3ca1f5b8c9
Cagri277 <53374310+Cagri277@users.noreply.github.com>
2020-04-04 20:49:26 +0300

    Delete homeWork1.js

--- homeWork1.js
+++
@@ -1,5 +0,0 @@
-let theBestNetflixSeries = [`Lucifer`, `LaCasaDePapel`, `StrangerThings`, `LockeAndKey`];
-
-console.log(theBestNetflixSeries);
-console.log(theBestNetflixSeries.length);
-console.log(theBestNetflixSeries[2]);
